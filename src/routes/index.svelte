746d690e98cdaab781fddbf245717b2ec8227ffb
Martin Ström <zicho.calypso@gmail.com>
2022-03-09 19:10:26 +0100

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+import Button from "@smui/button"
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Button on:click={() => alert('Clicked')}>Button</Button>
